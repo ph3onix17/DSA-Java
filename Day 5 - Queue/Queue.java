37f4a668265b2af2c0e448b5772efa22ab411008
ph3onix17 <chathuramaheshdharmasiride.com>
2024-10-22 18:48:30 +0530

    Queue

---
+++ Day 5 - Queue/Queue.java
@@ -0,0 +1,13 @@
+class Queue{
+    int front, rear;
+    int QueueArray[];
+    int size;
+
+    Queue(int size){
+        this.size = size;
+    }
+
+    public static void main(String[] args) {
+        Queue q = new Queue(5);
+    }
+}
